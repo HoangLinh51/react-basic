21c617c5c43c8aa8ec5f679e4d51811697d85708
HoangLinh51 <linhle5112002@gmail.com>
2023-05-05 15:11:25 +0700

    first commit

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+
+    render() {
+        return (
+            <div>Hello, it is my Component</div>
+        )
+
+    }
+}
+
+export default MyComponent;
